92436e96a356dc499a071ac5a784ffa52042dfc3
Brayan1951 <brayanc1951@gmail.com>
2024-08-22 23:17:50 -0500

    lista add

--- src/pages/Carrito/CarritoPage.jsx
+++ src/pages/Carrito/CarritoPage.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from 'react'
 
+import './carrito.css'
+
 export function CarritoPage({ carrito = [], setCarrito }) {
 
   const deleteCarrito = (index) => {
 
-    const newCarrito = carrito.filter((item,indx) =>  indx!==index)
+    const newCarrito = carrito.filter((item, indx) => indx !== index)
     setCarrito(newCarrito)
-    
+
 
   }
 
 
 
   return (
-    <section>
+    <section className='carrito-detalle'>
 
+      <div className="carrito-datos">
+        <h2>datos</h2>
+      </div>
 
-
+      <div className="carrito-total">
+        <h2>Total</h2>
+      </div>
       <div className="carrito-lista">
         <h3>Carrito de Compras (cantidad de productos: {carrito.length}) </h3>
         <ul>
